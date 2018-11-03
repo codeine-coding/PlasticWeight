94028edabe765852dd9895efa3ffd38e19df5dbf
Allen Whearry <allen@codeinecoding.com>
2018-11-03 12:52:19 -0400

    Initial Commit

---
+++ PlasticWeightTests/PlasticWeightTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlasticWeightTests.swift
+//  PlasticWeightTests
+//
+//  Created by Allen Whearry on 11/3/18.
+//  Copyright © 2018 Codeine Technologies LLC. All rights reserved.
+//
+
+import XCTest
+@testable import PlasticWeight
+
+class PlasticWeightTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
