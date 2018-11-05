7d79dac3f52091ef97b6a5d181fb83150537bf43
Allen Whearry <allen@codeinecoding.com>
2018-11-05 05:44:43 -0500

    add edge to edge constraint extension to UIView

--- PlasticWeight/Utils/Extensions.swift
+++ PlasticWeight/Utils/Extensions.swift
@@ -40,4 +40,13 @@ extension UIView {
         label.text = errorMessge
         hiddenLabel.isHidden = true
     }
+    
+    func edgeToEdgeConstraints(in view: UIView) {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: view.topAnchor),
+            self.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            self.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
 }
