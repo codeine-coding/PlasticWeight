fe9ac60f1db5cec7cdb755b221367630bd816a4b
Allen Whearry <allen@codeinecoding.com>
2018-11-05 04:38:55 -0500

    added navigation global appearance

--- PlasticWeight/AppDelegate.swift
+++ PlasticWeight/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let navigationAppearance = UINavigationBar.appearance()
+        navigationAppearance.barTintColor = .primary
+        navigationAppearance.prefersLargeTitles = true
+        navigationAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationAppearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         let mainTabController = MainTabBarController()
