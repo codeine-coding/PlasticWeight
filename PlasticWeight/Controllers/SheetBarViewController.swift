ff3787d8fcb0c72fbb6fe0e31277e2668c34a739
Allen Whearry <allen@codeinecoding.com>
2018-11-05 05:13:55 -0500

    Implemented SheetBar VC and tests

--- PlasticWeight/Controllers/SheetBarViewController.swift
+++ PlasticWeight/Controllers/SheetBarViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class SheetBarViewController: UIViewController {
+    var gaugeTextField: DimensionTextField!
+    var widthTextfield: DimensionTextField!
+    var lengthTextField: DimensionTextField!
+    var dimensionFields: [DimensionTextField]!
+    
+    var sheetBarView: UniversalView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        /// Set navigation bar
+        //
+        /// set bar style to light color
+        /// because this view sits within a navigation controller, you must set the navigation bar style
+        /// to black translucent (white) rather than using "preferredStatusBarStyle"
+        navigationController?.navigationBar.barStyle = .blackTranslucent
+        navigationItem.title = "Sheet / Bar"
+        
+        gaugeTextField = DimensionTextField(titleForLabel: "(T)hickness")
+        widthTextfield = DimensionTextField(titleForLabel: "(W)idth")
+        lengthTextField = DimensionTextField(titleForLabel: "(L)ength")
+        
+        dimensionFields =  [gaugeTextField, widthTextfield, lengthTextField]
+        
+        sheetBarView = UniversalView(dimensionFields: dimensionFields, calculation: .sheetBar)
+        sheetBarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(sheetBarView)
+        
+        NSLayoutConstraint.activate([
+            sheetBarView.topAnchor.constraint(equalTo: view.topAnchor),
+            sheetBarView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            sheetBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            sheetBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
     }
 
 }
