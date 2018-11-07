51de061b46afa0dfe585b2e1edda44b9c3729e5a
Allen Whearry <allen@codeinecoding.com>
2018-11-07 07:46:14 -0500

    implement screenshots using fastlane

--- PlasticWeightUITests/PlasticWeightUITests.swift
+++ PlasticWeightUITests/PlasticWeightUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class PlasticWeightUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,7 +18,9 @@ class PlasticWeightUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -26,9 +29,17 @@ class PlasticWeightUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGetSnapshots() {
+        
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons["Sheet / Bar"].tap()
+        snapshot("01SheetBarView")
+        tabBarsQuery.buttons["Rod"].tap()
+        snapshot("02RodView")
+        tabBarsQuery.buttons["Round Tube"].tap()
+        snapshot("03RoundTubeView")
+        tabBarsQuery.buttons["Square Tube"].tap()
+        snapshot("04SquareTubeView")
     }
 
 }
