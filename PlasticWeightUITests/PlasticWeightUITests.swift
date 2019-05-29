6447cad3673f28c079090aa78aa941298491f692
Allen Whearry <allen@Buddha-Pro-15.local>
2019-05-29 19:56:32 -0400

    initial UI Tests

--- PlasticWeightUITests/PlasticWeightUITests.swift
+++ PlasticWeightUITests/PlasticWeightUITests.swift
@@ -7,30 +7,74 @@
 //
 
 import XCTest
+@testable import PlasticWeight
 
 class PlasticWeightUITests: XCTestCase {
     var app: XCUIApplication!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app = XCUIApplication()
-        setupSnapshot(app)
-        app.launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
+    }
+
+    func testSheetBarVC_Exists_AndIsFirstViewLoaded() {
+        app.launch()
+        XCTAssertTrue(app.navigationBars["Sheet / Bar"].exists, "Sheet Bar View Should be first view controller")
+
+    }
+
+    func testRodVC_Exists() {
+        app.launch()
+        let title = "Rod"
+        app.tabBars.buttons[title].tap()
+        XCTAssertFalse(app.navigationBars[title].exists)
+    }
+
+    func testRoundTubeVC_Exists() {
+        app.launch()
+        let title = "Round Tube"
+        app.tabBars.buttons[title].tap()
+        XCTAssertFalse(app.navigationBars[title].exists)
+    }
+
+    func testSquareTubeVC_Exists() {
+        app.launch()
+        let title = "Square Tube"
+        app.tabBars.buttons[title].tap()
+        XCTAssertFalse(app.navigationBars[title].exists)
+    }
+
+    func testAlertView_WhenSelectedMaterial_IsEmpty() {
+
+        app.launch()
+        let sheetView = app.otherElements["SheetView"]
+        XCTAssertTrue(sheetView.exists)
+//        let textField = sheetView.descendants(matching: .textField)
+        let materialSelect = app.textFields["Select Material"]
+        XCTAssertTrue(materialSelect.exists)
+        materialSelect.tap()
+        let picker = sheetView.descendants(matching: .picker).element
+        XCTAssertTrue(picker.exists)
+
+//        picker.children(matching: .pickerWheel).element.swipeUp()
+//        app.pickers.children(matching: .pickerWheel).element.swipeUp()
+
+//        let app = XCUIApplication()
+//        app.textFields["Select Material"].tap()
+//        app.pickers.children(matching: .pickerWheel).element.swipeUp()
+//        app.toolbars["Toolbar"].buttons["Done"].tap()
+
+
     }
 
     func testGetSnapshots() {
-        
+        setupSnapshot(app)
+        app.launch()
         let tabBarsQuery = app.tabBars
         tabBarsQuery.buttons["Sheet / Bar"].tap()
         snapshot("01SheetBarView")
